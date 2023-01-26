b923b5a7cf6693bdcc24857aa5df6f3d874f48cf
andy <acbeers1@gmail.com>
2023-01-25 22:36:38 -0500

    made graph colors align with categories
    feature:
    - align chart colors with categories

--- OnePercent/OnePercent/v1.0/Data/GraphsView.swift
+++ OnePercent/OnePercent/v1.0/Data/GraphsView.swift
@@ -20,23 +20,93 @@ struct GraphsView: View {
                x: .value("Month", $0.date),
                y: .value("Value", $0.value))
            .foregroundStyle(by: .value("Name", $0.name))
+           .interpolationMethod(.catmullRom)
        }
        .frame(width: 330, height: 200)
        .chartYScale(domain: minValue...maxValue)
-       //.chartForegroundStyleScale(CreateLineStyleDictionary(categories: categories))
+       .chartForegroundStyleScale(CreateLineStyleDictionary(categories: categories))
     }
         
-//    func CreateLineStyleDictionary(categories:[Category]) -> KeyValuePairs<String, Color> {
-//        var pairs = KeyValuePairs<String, Color>()
-//        var listOfPairs:[KeyValuePair<String,Color>] = []
-//
-//        KeyValuePair<String, Color> keyValuePair = new KeyValuePair<String, Color>(1, 2);
-//        for category in categories {
-//            listOfPairs.append(new KeyValuePair<String, Color>(category.name, category.theme.mainColor))
-//        }
-//
-//        return new KeyValuePairs<String,Color>(listOfPairs)
-//    }
+    func CreateLineStyleDictionary(categories:[Category]) -> KeyValuePairs<String, Color> {
+        if (categories.count == 0){
+            return [:]
+        }
+        else if (categories.count == 1){
+            return [categories[0].name : categories[0].theme.mainColor]
+        }
+        else if (categories.count == 2){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor]
+        }
+        else if (categories.count == 3){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor]
+        }
+        else if (categories.count == 4){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor]
+        }
+        else if (categories.count == 5){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor]
+        }
+        else if (categories.count == 6){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor,
+                    categories[5].name : categories[5].theme.mainColor]
+        }
+        else if (categories.count == 7){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor,
+                    categories[5].name : categories[5].theme.mainColor,
+                    categories[6].name : categories[6].theme.mainColor]
+        }
+        else if (categories.count == 8){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor,
+                    categories[5].name : categories[5].theme.mainColor,
+                    categories[6].name : categories[6].theme.mainColor,
+                    categories[7].name : categories[7].theme.mainColor]
+        }
+        else if (categories.count == 9){
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor,
+                    categories[5].name : categories[5].theme.mainColor,
+                    categories[6].name : categories[6].theme.mainColor,
+                    categories[7].name : categories[7].theme.mainColor,
+                    categories[8].name : categories[8].theme.mainColor]
+        }
+        else {
+            return [categories[0].name : categories[0].theme.mainColor,
+                    categories[1].name : categories[1].theme.mainColor,
+                    categories[2].name : categories[2].theme.mainColor,
+                    categories[3].name : categories[3].theme.mainColor,
+                    categories[4].name : categories[4].theme.mainColor,
+                    categories[5].name : categories[5].theme.mainColor,
+                    categories[6].name : categories[6].theme.mainColor,
+                    categories[7].name : categories[7].theme.mainColor,
+                    categories[8].name : categories[8].theme.mainColor,
+                    categories[9].name : categories[9].theme.mainColor]
+        }
+    }
 }
 
 struct GraphsView_Previews: PreviewProvider {
